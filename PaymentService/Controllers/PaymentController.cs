3656ec43ca82933076ee3b3ba923b3d6ea9ff237
Levent Gürsoy Tonkal <gursoy.tonkal@bil.omu.edu.tr>
2020-02-18 14:35:13 +0300

    initial commit

---
+++ PaymentService/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet("get")]
+        public string Get()
+        {
+            return "This Payment Service";
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("get")]
+        public string Get()
+        {
+            return "This Product Service";
+        }
+    }
+}
